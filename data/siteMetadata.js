c599e50db71a20ec6d435025ba6771495e467766
muyiwa johnson <75730906+codewithjohnson@users.noreply.github.com>
2025-04-22 13:36:29 +0100

    Refactor code structure for improved readability and maintainability

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -7,9 +7,9 @@ const siteMetadata = {
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteRepo: 'https://github.com/codewithjohnson/portfolio-v2',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
-  socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
+  socialBanner: `${process.env.BASE_PATH || ''}/static/images/banner.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'codewithjohnson@gmail.com',
   github: 'https://github.com/codewithjohnson',
